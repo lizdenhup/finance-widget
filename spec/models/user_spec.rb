ec001f1fe3b7932e3937d2b3f5b7dc2d1cc3f318
lizdenhup <lizdenhup@gmail.com>
2017-05-24 13:42:34 -0500

    added initial user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
+  describe 'validations' do 
+    it 'requires email and password upon creation'
+    it 'requires that an email is unique'
+  end 
+  describe 'relationships' do
+    it 'has many stocks'
+  end 
 end
