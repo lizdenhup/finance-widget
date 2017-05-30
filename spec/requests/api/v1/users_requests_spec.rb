23f16ea9855e30fb0f05a2eb23ed7f31adcc3bc5
lizdenhup <lizdenhup@gmail.com>
2017-05-30 09:17:19 -0500

    finished users_requests spec

--- spec/requests/api/v1/users_requests_spec.rb
+++ spec/requests/api/v1/users_requests_spec.rb
@@ -1,13 +1,62 @@
 require 'rails_helper'
 
-RSpec.describe 'API::V1::Users', type: :request do 
+RSpec.describe "Api::V1::Users", type: :request do 
 
-  describe 'POST /users' do 
-    describe 'on success' do 
-    it 'creates a user from the params'
+  describe "POST /users" do 
+    describe "on success" do 
 
-    it 'returns the new user and a JWT token'
-    end  
-  end 
+      before(:each) do  
+        params = { 
+          user: { 
+            email: 'test@gmail.com', 
+            password: 'password' 
+          } 
+        }
 
-end 
+        post "/api/v1/users", 
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+          
+        @response = response
+      end
+      
+      it "creates a user from the params" do
+        expect(User.all.count).to eq(1)
+      end
+
+      it "returns the new user and a JWT token" do  
+        body = JSON.parse(@response.body)
+
+        expect(@response.status).to eq(200)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).to eq('test@gmail.com')
+        expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)  
+      end
+    end
+
+    describe "on error" do
+
+      it "required a valid email and password" do  
+        params = { 
+          user: { 
+            email: '', 
+            password: '' 
+          } 
+        }
+
+        post "/api/v1/users", 
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+          
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(500)
+        expect(body["errors"]).to eq({
+          "password"=>["can't be blank"], 
+          "email"=>["can't be blank", "is invalid"]
+        })
+      end
+    end
+  end
+end
