02c98ac76c8d1220b6f70a5d2857e880218bea25
lizdenhup <lizdenhup@gmail.com>
2017-05-30 09:10:34 -0500

    add spec for user_requests

---
+++ spec/requests/api/v1/users_requests_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'API::V1::Users', type: :request do 
+
+  describe 'POST /users' do 
+    describe 'on success' do 
+    it 'creates a user from the params'
+
+    it 'returns the new user and a JWT token'
+    end  
+  end 
+
+end 
