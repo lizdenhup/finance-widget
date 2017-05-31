fc7f2ba787253f4ca6cb2e878fe309ff1a0f483f
lizdenhup <lizdenhup@gmail.com>
2017-05-31 09:49:28 -0500

    added Auth.decode_token

--- app/lib/auth.rb
+++ app/lib/auth.rb
@@ -5,4 +5,8 @@ class Auth
     JWT.encode(payload, ENV['AUTH_SECRET'], ENV['AUTH_ALGORITHM'])
   end
 
+  def self.decode_token(token)
+    JWT.decode(token, ENV['AUTH_SECRET'], true, { algorithm: ENV['AUTH_ALGORITHM'] })
+  end
+
 end 
