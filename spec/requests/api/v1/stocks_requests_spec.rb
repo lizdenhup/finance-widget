a224010360c58e18e32ee043e44fa780ac7575ae
lizdenhup <lizdenhup@gmail.com>
2017-05-31 12:08:04 -0500

    adding to stocks spec

--- spec/requests/api/v1/stocks_requests_spec.rb
+++ spec/requests/api/v1/stocks_requests_spec.rb
@@ -2,5 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Stocks", type: :request do
 
-  it 'requires all routes to have a token'
+  before(:each) do 
+    @user = create(:user)
+    @token = Auth.create_token(@user.id)
+    @token_headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': "Bearer: #{token}"
+    }
+    @tokenless_headers = {
+      'Content-Type': 'application/json'
+    }
+  end
+
+  # it 'requires all routes to have a token' do 
+  #   responses = []
+
+  #   post '/api/v1/stocks', params: {
+      
+  #   }
+  # end 
+
 end 
