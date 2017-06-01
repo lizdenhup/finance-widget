b21cb2b0542aad7e03ba826c69a175de724a90f8
lizdenhup <lizdenhup@gmail.com>
2017-06-01 18:37:13 -0500

    working Rails API with Alpha Vantage API integration

--- api/app/services/stock_service.rb
+++ api/app/services/stock_service.rb
@@ -1,11 +1,9 @@
 class StockService 
 
   def fetch_stock(stock_symbol)
-    # stock_symbol = 'GOOGL'
     @response = Faraday.get 'http://www.alphavantage.co/query?' do |req|
       req.params['function'] = 'TIME_SERIES_DAILY'
       req.params['symbol'] = stock_symbol
-      # will change this symbol to a var once I link the search form to this API call
       req.params['apikey'] = ENV['ALPHA_VANTAGE_API_KEY']
     end
     @response.body
