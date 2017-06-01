d388af2b5606430daf3280c1f92b5f7a86f2158b
lizdenhup <lizdenhup@gmail.com>
2017-06-01 17:16:50 -0500

    added services folder to autloaded paths, trying to get search working

--- api/app/controllers/api/v1/searches_controller.rb
+++ api/app/controllers/api/v1/searches_controller.rb
@@ -1,3 +1,10 @@
 class Api::V1::SearchesController < ApplicationController
 
+def search
+  fetch_stocks = StockService.new
+  fetch_stock(params[:query])
+end 
+
+
+
 end 
--- api/config/application.rb
+++ api/config/application.rb
@@ -21,6 +21,7 @@ module Api
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('services')
     config.api_only = true
   end
 end
