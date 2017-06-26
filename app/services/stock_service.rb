6fa375f3f30ce05af8d7e0450d36dceaf2900a83
lizdenhup <lizdenhup@gmail.com>
2017-06-26 10:12:56 -0500

    changed http to https in stockService bc of external API changes

--- app/services/stock_service.rb
+++ app/services/stock_service.rb
@@ -1,7 +1,7 @@
 class StockService 
 
   def fetch_stock(stock_symbol)
-    @response = Faraday.get 'http://www.alphavantage.co/query?' do |req|
+    @response = Faraday.get 'https://www.alphavantage.co/query?' do |req|
       req.params['function'] = 'TIME_SERIES_DAILY'
       req.params['symbol'] = stock_symbol
       req.params['apikey'] = ENV['ALPHA_VANTAGE_API_KEY']
