04f912f491a731e52ebc0812b6397e2108501d15
lizdenhup <lizdenhup@gmail.com>
2017-07-17 14:39:26 -0500

    added stock-related routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do 
       # /api/v1/users
       # /api/v1/users/:user_id 
-      resources :users, only: [:create, :show]
-
+      resources :users, only: [:create, :show] do 
+        resources :stocks, only: [:index, :show, :create, :update, :destroy]
+      end 
       # /api/v1/auth
       post '/auth', to: "auth#login"
       post '/auth/refresh', to: "auth#refresh"
