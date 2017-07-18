7b3d94c3ffa34ed4585cbf6a5d9f61d99e0c8056
lizdenhup <lizdenhup@gmail.com>
2017-07-18 15:37:07 -0500

    added create, show, destroy actions to stocks_controller

--- app/controllers/api/v1/stocks_controller.rb
+++ app/controllers/api/v1/stocks_controller.rb
@@ -1,9 +1,55 @@
 class Api::V1::StocksController < ApplicationController
+before_action :authenticate_token!, only: [:create, :update, :destroy]
+
+  def index
+    # @stocks = User.find_by(id: params[:user_id])&.stocks
+    # render 'stocks/stocks.json.jbuilder', stocks: @stocks
+  end
+
+  def show
+    @stock = Stock.find_by(id: params[:id])
+    if @stock
+      render 'stocks/stock.json.jbuilder', stock: @stock
+    else
+      render json: {
+        errors: {
+          message: "Page not found"
+        }
+      }, status: 404
+    end
+  end
+
+  def create
+    @user = User.find_by(id: params[:user_id])
+    if @user.id == current_user.id
+      @stock = @user.stocks.new(stock_params)
+      if @stock.save
+        render 'stocks/stocks.json.jbuilder', stock: @stock
+      else
+        render json: {
+          errors: @stock.errors.full_messages
+        }, status: 500
+      end
+  end
+
+  def destroy
+    stock = Stock.find_by(id: params[:id])
+    if stock
+      stock.destroy
+      head :no_content
+    else
+      render json: {
+        errors: {
+          message: "Page not found"
+        }
+      }, status: 404
+    end
+  end
 
   private 
 
     def stock_params 
-      params.require(:stock).permit(:name)
+      params.require(:stock).permit(:name, :id)
     end
 end
 
